eb69ad3c780ab2217b870c721af241b056339a93
jiangzongwen <jiangzongwen@project.com>
2014-03-31 11:48:39 +0800

    修改jar包apk转成jd可读包shell

--- jar_unzip_dex.sh
+++ jar_unzip_dex.sh
@@ -13,16 +13,18 @@ tmp_folder="tmp"
 cd $file_folder
 for file in `ls`
 do
-	file_name=`basename $file`
-	file_name=${file_name%.*}
-	#echo "file_name=$file_name file=$file"
+	file_tmp=`basename $file`
+	file_name=${file_tmp%.*}
+	file_type=${file_tmp##*.}
+	#echo "file_name=$file_name file=$file file_type=$file_type"
+
 	unzip $file -d $tmp_folder
 	#echo "$PORT_ROOT/dex2jar/dex2jar.sh $tmp_folder/classes.dex"
 	$PORT_ROOT/dex2jar/dex2jar.sh $tmp_folder/classes.dex
 	#echo "$tmp_folder/classes_dex2jar.jar"
 	if [ -f "$tmp_folder/classes_dex2jar.jar" ]
 	then
-		rm -f $file_name.jar
+		rm -f $file_name.$file_type
 		mv $tmp_folder/classes_dex2jar.jar $file_name.jar
 	fi
 	rm -rf $tmp_folder
