5058b5afbafaba4883a3e1770e820ead9614df55
jiangzongwen <jiangzongwen@project.com>
2014-10-11 16:38:09 +0800

    svn commit 脚本

---
+++ shell/svn_commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#需要提交的文件路径
+file_list="\
+packages/apps/WriteImei \
+"
+
+#提交的日志
+message="添加手动修改imei单卡应用"
+
+
+
+echo $message > msg_tmp.txt
+
+svn commit -F msg_tmp.txt $file_list
+
+rm -f msg_tmp.txt
